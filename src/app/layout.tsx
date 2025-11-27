5681636cd6799a6eec3b3ab60a3aa444af24417f
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-11-27 13:23:12 +0700

    feat: menambahkan halaman depan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,12 @@
+/* 
+  layout merupakan komponen yang membungkus semua halaman di dalam folder tertentu. untuk pertama kalinya layout ini akan menjadi pembungkus dari page.tsx yang sudah secara otomatis dibuat oleh nextjs ketika membuat project baru
+
+  bisa juga dalam folder tertentu dibuat layout lagi yang akan membungkus halaman-halaman di dalam folder tersebut
+
+  misalnya di dalam folder src/app dibuat folder blog, maka di dalam foldeer blog tersebut bida dibuat layout.tsx lagi yang akan memubungkus halaman page.tsx di dalam folder blog tersebut.
+
+*/
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -11,6 +20,10 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+/*
+  metadata ini adalah fitur dari nextjs untuk mengatur metadata dari halaman web seperti title, description, dan lain-lain secara terstruktur. metadata ini berguna untuk SEO (Search Engine Optimization) dan untuk memberikan informasi yang lebih baik tentang halaman web kepada mesin pencari dan browser.
+*/
 
 export const metadata: Metadata = {
   title: "Next JS News App",
