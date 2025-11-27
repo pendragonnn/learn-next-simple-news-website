32d2f09f224ffab1d480cf3b01a2aa5e8ab98ecf
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-11-27 13:47:32 +0700

    feat: menambahkan routing untuk halaman about dan berpindah menggunakan Link

---
+++ src/app/about/page.jsx
@@ -0,0 +1,38 @@
+/*
+  menambahkan route about pada nextjs caranya cukup mudah yaitu dengan membuat folder baru di dalam folder app dengan nama sesuai dengan yang diinginkan (misalnya about) kemudian di dalam folder about tersebut dibuat file page.jsx atau page.tsx yang akan menjadi halaman utama dari route about tersebut.
+*/
+
+function page() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
+        provident dolorum alias nihil tempora quia sunt atque tempore facere
+        maiores. Nihil ullam recusandae harum quod, reiciendis voluptatum optio
+        provident nobis quibusdam ipsam officia aliquid enim laudantium quam
+        perspiciatis accusantium dicta ab iste dolorum praesentium tempora non
+        consequuntur facilis quas. Esse asperiores enim doloremque at, itaque
+        cumque incidunt obcaecati repellendus assumenda! Necessitatibus numquam
+        adipisci, expedita perspiciatis nam quas. Voluptatum animi dolores
+        doloribus velit provident repudiandae facilis harum? Distinctio impedit
+        numquam facilis dolorum nemo! Tenetur reiciendis repellendus nisi iure
+        facere repellat necessitatibus. Facere veritatis amet corrupti
+        distinctio fugit quae atque, iste natus veniam perspiciatis aliquam
+        totam tempora soluta! Quia et accusantium, ratione obcaecati recusandae
+        sint ullam consequuntur aliquam inventore aperiam velit culpa quibusdam
+        architecto autem deleniti molestias soluta numquam suscipit cupiditate
+        nemo reiciendis, temporibus laudantium nihil tempore. Repellendus
+        incidunt ea eum. Minus consequuntur consectetur iure animi error harum
+        repellat sunt doloribus ipsa architecto rem eligendi ipsam, nemo autem
+        eum illo minima id. Autem illo sit ad vero dolorum et pariatur impedit
+        architecto eius, molestias exercitationem obcaecati perspiciatis
+        laudantium commodi libero magnam odio ducimus dolore eum atque
+        perferendis quae. Id adipisci, earum aliquid rerum odio consectetur
+        itaque voluptates dolor deleniti enim, repellat obcaecati!
+      </p>
+    </>
+  );
+}
+
+export default page;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
+/*
+  komponen Link adalah komponen bawaan Next.js yang dipakai untuk membuat navigasi antar halaman tanpa reluad penuh (client-side navigation). 
+  komponen ini:
+  - hanya mengganti konten halaman
+  - tidak memuat ulang CSS/JS seluruhnya
+  - tidak menghapus state global (misalnya context)
+  - mengambil data halaman terlebih dahulu, bahkan sebelum user klik
+  - langsung bekerja dengan sistem routing Next.js 
+*/
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="header">
       <nav>
         <div className="logo">
-          <a href="">Kasih Info</a>
+          <Link href="/">Kasih Info</Link>
+        </div>
+        <div className="nav-links">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
         </div>
       </nav>
     </header>
